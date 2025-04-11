edba0d09d790c5a1c2fefe5b4f47cc0a2644b313
A1ishka <alinamakogon123456@gmail.com>
2025-04-11 16:11:34 +0300

    Reg_Сhange fragments' dependencies' package name init 3

--- app/src/androidTest/java/by/dis/birdvoice/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/by/dis/birdvoice/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("by.ssrlab.birdvoice", appContext.packageName)
+        assertEquals("by.dis.birdvoice", appContext.packageName)
     }
 }
